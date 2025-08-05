7060e43a1bebae48ac656f3fefff3b027be1d96a
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-08-05 10:57:18 +0530

    refactor: move commands to commands.lua

--- lua/markit.lua
+++ lua/markit.lua
@@ -1,7 +1,10 @@
 local mark = require('markit.mark')
 local bookmark = require('markit.bookmark')
 local utils = require('markit.utils')
+local commands = require('markit.commands')
+local config = require('markit.config')
 local pickme = require('pickme')
+
 local M = {}
 
 function M.set()
@@ -549,204 +552,65 @@ function M.bookmarks_list_group(group_nr)
     end)
 end
 
-M.mappings = {
-    set = 'm',
-    set_next = 'm,',
-    toggle = 'm;',
-    toggle_mark = 'M',
-    next = 'm]',
-    prev = 'm[',
-    preview = 'm:',
-    next_bookmark = 'm}',
-    prev_bookmark = 'm{',
-    delete = 'dm',
-    delete_line = 'dm-',
-    delete_bookmark = 'dm=',
-    delete_buf = 'dm<space>',
-}
-
-for i = 0, 9 do
-    M.mappings['set_bookmark' .. i] = 'm' .. tostring(i)
-    M.mappings['delete_bookmark' .. i] = 'dm' .. tostring(i)
-end
-
-local function user_mappings(config)
-    for cmd, key in pairs(config.mappings) do
-        if key ~= false then
-            M.mappings[cmd] = key
-        else
-            M.mappings[cmd] = nil
-        end
-    end
-end
-
-local function apply_mappings()
-    for cmd, key in pairs(M.mappings) do
-        vim.api.nvim_set_keymap('n', key, '', { callback = M[cmd], desc = 'markit: ' .. cmd:gsub('_', ' ') })
-    end
-end
-
-local function setup_highlights()
-    vim.api.nvim_set_hl(0, 'MarkSignHL', { link = 'Identifier', default = true })
-    vim.api.nvim_set_hl(0, 'MarkSignLineHL', { link = 'NONE', default = true })
-    vim.api.nvim_set_hl(0, 'MarkSignNumHL', { link = 'CursorLineNr', default = true })
-    vim.api.nvim_set_hl(0, 'MarkVirtTextHL', { link = 'Comment', default = true })
-end
-
--- Setup commands
-local function setup_commands()
-    -- Marks commands
-    vim.api.nvim_create_user_command('MarksToggleSigns', function(opts)
-        require('markit').toggle_signs(opts.args)
-    end, { nargs = '?' })
-
-    vim.api.nvim_create_user_command('MarksListBuf', function()
-        require('markit').marks_list_buf()
-    end, {})
-
-    vim.api.nvim_create_user_command('MarksListGlobal', function()
-        require('markit').marks_list_all()
-    end, {})
-
-    vim.api.nvim_create_user_command('MarksListAll', function()
-        require('markit').marks_list_all()
-    end, {})
-
-    -- Marks quickfix commands
-    vim.api.nvim_create_user_command('MarksQFListBuf', function()
-        require('markit').mark_state:buffer_to_list('quickfixlist')
-        vim.cmd('copen')
-    end, {})
-
-    vim.api.nvim_create_user_command('MarksQFListGlobal', function()
-        require('markit').mark_state:global_to_list('quickfixlist')
-        vim.cmd('copen')
-    end, {})
-
-    vim.api.nvim_create_user_command('MarksQFListAll', function()
-        require('markit').mark_state:all_to_list('quickfixlist')
-        vim.cmd('copen')
-    end, {})
-
-    -- Bookmarks commands
-    vim.api.nvim_create_user_command('BookmarksList', function(opts)
-        local group_nr = tonumber(opts.args)
-        if group_nr then
-            require('markit').bookmarks_list_group(group_nr)
-        else
-            require('markit').bookmarks_list_all()
-        end
-    end, { nargs = '?' })
-
-    vim.api.nvim_create_user_command('BookmarksListAll', function()
-        require('markit').bookmarks_list_all()
-    end, {})
-
-    -- Bookmarks quickfix commands
-    vim.api.nvim_create_user_command('BookmarksQFList', function(opts)
-        require('markit').bookmark_state:to_list('quickfixlist', tonumber(opts.args))
-        vim.cmd('copen')
-    end, { nargs = 1 })
-
-    vim.api.nvim_create_user_command('BookmarksQFListAll', function()
-        require('markit').bookmark_state:all_to_list('quickfixlist')
-        vim.cmd('copen')
-    end, {})
-end
-
-local function setup_mappings(config)
-    if not config.default_mappings then
-        M.mappings = {}
-    end
-    if config.mappings then
-        user_mappings(config)
-    end
-    apply_mappings()
-end
-
-local function setup_autocommands()
-    vim.cmd([[augroup Marks_autocmds
-    autocmd!
-    autocmd BufEnter * lua require'markit'.refresh(true)
-    autocmd CursorHold * lua require'markit'.refresh()
-    autocmd BufDelete * lua require'markit'._on_delete()
-    autocmd VimLeavePre * lua require'markit'.bookmark_state:save()
-    autocmd DirChanged * lua require'markit'.bookmark_state:load()
-  augroup end]])
-end
-
-function M.setup(config)
-    config = config or {}
-
+local function assign_defaults()
+    local config_options = config.config
     M.mark_state = mark.new()
-    M.mark_state.builtin_marks = config.builtin_marks or {}
+    M.mark_state.builtin_marks = config_options.builtin_marks
 
     M.bookmark_state = bookmark.new()
 
-    local bookmark_config
     for i = 0, 9 do
-        bookmark_config = config['bookmark_' .. i]
-        if bookmark_config then
-            if bookmark_config.sign == false then
-                M.bookmark_state.signs[i] = nil
-            else
-                M.bookmark_state.signs[i] = bookmark_config.sign or M.bookmark_state.signs[i]
-            end
-            M.bookmark_state.virt_text[i] = bookmark_config.virt_text or M.bookmark_state.virt_text[i]
-            M.bookmark_state.prompt_annotate[i] = bookmark_config.annotate
+        local bookmark_config = config_options['bookmark_' .. i]
+        if bookmark_config.sign == false then
+            M.bookmark_state.signs[i] = nil
+        else
+            M.bookmark_state.signs[i] = bookmark_config.sign
         end
+        M.bookmark_state.virt_text[i] = bookmark_config.virt_text
+        M.bookmark_state.prompt_annotate[i] = bookmark_config.annotate
     end
 
     local excluded_fts = {}
-    for _, ft in ipairs(config.excluded_filetypes or {}) do
+    for _, ft in ipairs(config_options.excluded_filetypes) do
         excluded_fts[ft] = true
     end
-
     M.excluded_fts = excluded_fts
 
     local excluded_bts = {}
-    for _, bt in ipairs(config.excluded_buftypes or {}) do
+    for _, bt in ipairs(config_options.excluded_buftypes) do
         excluded_bts[bt] = true
     end
-
     M.excluded_bts = excluded_bts
 
     M.bookmark_state.opt.signs = true
     M.bookmark_state.opt.buf_signs = {}
 
-    config.default_mappings = utils.option_nil(config.default_mappings, true)
-    config.add_default_keybindings = utils.option_nil(config.add_default_keybindings, true)
-
-    setup_mappings(config)
-    setup_highlights()
-    setup_commands()
-    setup_autocommands()
-
-    if config.add_default_keybindings then
-        local commands = require('markit.commands')
-        commands.setup_default_keybindings()
-    end
-
-    M.mark_state.opt.signs = utils.option_nil(config.signs, true)
+    M.mark_state.opt.signs = config_options.signs
     M.mark_state.opt.buf_signs = {}
-    M.mark_state.opt.force_write_shada = utils.option_nil(config.force_write_shada, false)
-    M.mark_state.opt.cyclic = utils.option_nil(config.cyclic, true)
+    M.mark_state.opt.force_write_shada = config_options.force_write_shada
+    M.mark_state.opt.cyclic = config_options.cyclic
 
     M.mark_state.opt.priority = { 10, 10, 10 }
     local mark_priority = M.mark_state.opt.priority
-    if type(config.sign_priority) == 'table' then
-        mark_priority[1] = config.sign_priority.lower or mark_priority[1]
-        mark_priority[2] = config.sign_priority.upper or mark_priority[2]
-        mark_priority[3] = config.sign_priority.builtin or mark_priority[3]
-        M.bookmark_state.priority = config.sign_priority.bookmark or 10
-    elseif type(config.sign_priority) == 'number' then
-        mark_priority[1] = config.sign_priority
-        mark_priority[2] = config.sign_priority
-        mark_priority[3] = config.sign_priority
-        M.bookmark_state.priority = config.sign_priority
+    if type(config_options.sign_priority) == 'table' then
+        mark_priority[1] = config_options.sign_priority.lower
+        mark_priority[2] = config_options.sign_priority.upper
+        mark_priority[3] = config_options.sign_priority.builtin
+        M.bookmark_state.priority = config_options.sign_priority.bookmark
+    elseif type(config_options.sign_priority) == 'number' then
+        mark_priority[1] = config_options.sign_priority
+        mark_priority[2] = config_options.sign_priority
+        mark_priority[3] = config_options.sign_priority
+        M.bookmark_state.priority = config_options.sign_priority
     end
 
     M.bookmark_state:load()
+end
+
+function M.setup(opts)
+    config.setup(opts)
+    commands.setup()
+    assign_defaults()
 end
 
 return M
