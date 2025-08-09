a82dd1220a4fc327f6b3d13448f7b5c78d336f3a
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-08-09 08:59:23 +0530

    feat: extract all icons to config

--- lua/markit/config.lua
+++ lua/markit/config.lua
@@ -1,21 +1,35 @@
 local M = {}
 
+---@class MarkitMarkIcons
+---@field buffer string
+---@field default string
+---@field global string
+---@field last_change string
+---@field last_insert string
+---@field last_jump string
+---@field numbered string
+---@field visual_end string
+---@field visual_start string
+
+---@class MarkitIcons
+---@field content_separator string
+---@field default_bookmark string
+---@field error string
+---@field file string
+---@field line_separator string
+---@field target string
+---@field marks MarkitMarkIcons
+
 ---@class MarkitBookmark
 ---@field sign string : Sign character to display (empty string to disable)
 ---@field virt_text string : Virtual text to show at end of line
 ---@field annotate boolean : Whether to prompt for annotation when setting bookmark
 
----@class MarkitIcons
----@field target string
----@field line_separator string
----@field content_separator string
-
 ---@class MarkitPreviewOptions
 ---@field context_before integer : How many lines to show before marked line
 ---@field context_after integer : How many lines to show after marked line
----@field icons MarkitIcons : Table of icons
 
----@class markit.config
+---@class MarkitConfig
 ---@field add_default_keybindings boolean : Whether to add comprehensive default keybindings
 ---@field builtin_marks table : Which builtin marks to show in sign column
 ---@field cyclic boolean : Whether movements cycle back to beginning/end of buffer
@@ -27,6 +41,8 @@ local M = {}
 ---@field signs boolean : Whether to show signs in sign column
 ---@field bookmarks MarkitBookmark[] : Array of bookmark group configurations
 ---@field preview MarkitPreviewOptions : Config options for markit preview
+---@field icons MarkitIcons : List of icons used by Markit
+
 M.config = {
     add_default_keybindings = true,
 
@@ -39,22 +55,37 @@ M.config = {
         bookmark = 20,
     },
 
-    preview = {
-        context_before = 10,
-        context_after = 20,
-        icons = {
-            target = ' ',
-            line_separator = ' │',
-            content_separator = '─',
-        },
-    },
-
     cyclic = true,
     force_write_shada = false,
     refresh_interval = 150,
 
     excluded_filetypes = {},
     excluded_buftypes = {},
+
+    preview = {
+        context_before = 10,
+        context_after = 20,
+    },
+
+    icons = {
+        content_separator = '─',
+        default_bookmark = ' ',
+        error = ' ',
+        file = ' ',
+        line_separator = ' │',
+        target = ' ',
+        marks = {
+            buffer = ' ',
+            default = ' ',
+            global = ' ',
+            last_change = ' ',
+            last_insert = ' ',
+            last_jump = ' ',
+            numbered = ' ',
+            visual_end = ' ',
+            visual_start = ' ',
+        },
+    },
 
     bookmarks = {
         { sign = ' ', virt_text = '', annotate = false },
