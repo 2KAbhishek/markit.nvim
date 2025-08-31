1d121ba18a15a70cf96e960f059b9bc5ea652001
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-08-31 21:01:42 +0530

    feat: use new commands for keybindings

--- lua/markit/commands.lua
+++ lua/markit/commands.lua
@@ -427,72 +427,72 @@ end
 
 local function setup_default_keybindings(config)
     local mappings = {
-        { '<leader>mm', ':lua require("markit").marks_list_all()<cr>', 'All Marks' },
-        { '<leader>mM', ':lua require("markit").marks_list_buf()<cr>', 'Buffer Marks' },
-        { '<leader>ms', ':lua require("markit").set_next()<cr>', 'Set Next Available Mark' },
-        { '<leader>mS', ':lua require("markit").set()<cr>', 'Set Mark (Interactive)' },
-        { '<leader>mt', ':lua require("markit").toggle()<cr>', 'Toggle Mark at Cursor' },
-        { '<leader>mT', ':lua require("markit").toggle_mark()<cr>', 'Toggle Mark (Interactive)' },
+        { '<leader>mm', ':Markit mark list all<cr>', 'All Marks' },
+        { '<leader>mM', ':Markit mark list buffer<cr>', 'Buffer Marks' },
+        { '<leader>ms', ':Markit mark set<cr>', 'Set Next Available Mark' },
+        { '<leader>mS', ':Markit mark set<cr>', 'Set Mark (Interactive)' },
+        { '<leader>mt', ':Markit mark toggle<cr>', 'Toggle Mark at Cursor' },
+        { '<leader>mT', ':Markit mark toggle<cr>', 'Toggle Mark (Interactive)' },
 
-        { '<leader>mj', ':lua require("markit").next()<cr>', 'Next Mark' },
-        { '<leader>mk', ':lua require("markit").prev()<cr>', 'Previous Mark' },
-        { '<leader>mP', ':lua require("markit").preview()<cr>', 'Preview Mark' },
+        { '<leader>mj', ':Markit mark next<cr>', 'Next Mark' },
+        { '<leader>mk', ':Markit mark prev<cr>', 'Previous Mark' },
+        { '<leader>mP', ':Markit mark preview<cr>', 'Preview Mark' },
 
-        { '<leader>md', ':lua require("markit").delete_line()<cr>', 'Delete Marks In Line' },
-        { '<leader>mD', ':lua require("markit").delete_buf()<cr>', 'Delete Marks In Buffer' },
-        { '<leader>mX', ':lua require("markit").delete()<cr>', 'Delete Mark (Interactive)' },
+        { '<leader>md', ':Markit mark delete line<cr>', 'Delete Marks In Line' },
+        { '<leader>mD', ':Markit mark delete buffer<cr>', 'Delete Marks In Buffer' },
+        { '<leader>mX', ':Markit mark delete<cr>', 'Delete Mark (Interactive)' },
 
-        { '<leader>mb', ':lua require("markit").bookmarks_list_all()<cr>', 'All Bookmarks' },
-        { '<leader>mx', ':lua require("markit").delete_bookmark()<cr>', 'Delete Bookmark at Cursor' },
-        { '<leader>ma', ':lua require("markit").annotate()<cr>', 'Annotate Bookmark' },
+        { '<leader>mb', ':Markit bookmark list all<cr>', 'All Bookmarks' },
+        { '<leader>mx', ':Markit bookmark delete<cr>', 'Delete Bookmark at Cursor' },
+        { '<leader>ma', ':Markit bookmark annotate<cr>', 'Annotate Bookmark' },
 
-        { '<leader>ml', ':lua require("markit").next_bookmark()<cr>', 'Next Bookmark' },
-        { '<leader>mh', ':lua require("markit").prev_bookmark()<cr>', 'Previous Bookmark' },
+        { '<leader>ml', ':Markit bookmark next<cr>', 'Next Bookmark' },
+        { '<leader>mh', ':Markit bookmark prev<cr>', 'Previous Bookmark' },
 
-        { '<leader>mv', ':lua require("markit").toggle_signs()<cr>', 'Toggle Signs' },
+        { '<leader>mv', ':Markit bookmark signs<cr>', 'Toggle Signs' },
 
-        { '<leader>mqm', ':MarksQFListAll<cr>', 'All Marks → QuickFix' },
-        { '<leader>mqb', ':BookmarksQFListAll<cr>', 'All Bookmarks → QuickFix' },
-        { '<leader>mqM', ':MarksQFListBuf<cr>', 'Buffer Marks → QuickFix' },
-        { '<leader>mqg', ':MarksQFListGlobal<cr>', 'Global Marks → QuickFix' },
+        { '<leader>mqm', ':Markit mark list quickfix all<cr>', 'All Marks → QuickFix' },
+        { '<leader>mqb', ':Markit bookmark list quickfix all<cr>', 'All Bookmarks → QuickFix' },
+        { '<leader>mqM', ':Markit mark list quickfix buffer<cr>', 'Buffer Marks → QuickFix' },
+        { '<leader>mqg', ':Markit mark list quickfix global<cr>', 'Global Marks → QuickFix' },
     }
 
     for i, _ in ipairs(config.bookmarks) do
         local group_index = i - 1
         table.insert(mappings, {
             string.format('<leader>m%d', group_index),
-            string.format(':lua require("markit").toggle_bookmark%d()<cr>', group_index),
+            string.format(':Markit bookmark toggle %d<cr>', group_index),
             string.format('Toggle Group %d Bookmark', group_index),
         })
 
         table.insert(mappings, {
             string.format('<leader>mp%d', group_index),
-            string.format(':lua require("markit").prev_bookmark%d()<cr>', group_index),
+            string.format(':Markit bookmark prev %d<cr>', group_index),
             string.format('Previous Group %d Bookmark', group_index),
         })
 
         table.insert(mappings, {
             string.format('<leader>mn%d', group_index),
-            string.format(':lua require("markit").next_bookmark%d()<cr>', group_index),
+            string.format(':Markit bookmark next %d<cr>', group_index),
             string.format('Next Group %d Bookmark', group_index),
         })
 
         table.insert(mappings, {
             string.format('<leader>mg%d', group_index),
-            string.format(':lua require("markit").bookmarks_list_group(%d)<cr>', group_index),
+            string.format(':Markit bookmark list %d<cr>', group_index),
             string.format('Group %d Bookmarks', group_index),
         })
 
         table.insert(mappings, {
             string.format('<leader>mq%d', group_index),
-            string.format(':BookmarksQFList %d<cr>', group_index),
+            string.format(':Markit bookmark list quickfix %d<cr>', group_index),
             string.format('Group %d Bookmarks → QuickFix', group_index),
         })
 
         table.insert(mappings, {
             string.format('<leader>mc%d', group_index),
-            string.format(':lua require("markit").delete_bookmark%d()<cr>', group_index),
-            string.format('Clear Group %d Bookmarks', group_index),
+            string.format(':Markit bookmark delete %d<cr>', group_index),
+            string.format('Delete Group %d Bookmarks', group_index),
         })
     end
 
